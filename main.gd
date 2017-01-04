ccc5739b470bb0c194011104bdeadeff79cb5115
radubolovan <radubolovan@gmail.com>
2017-01-04 22:18:18 +0200

    Adding project files

---
+++ main.gd
@@ -0,0 +1,10 @@
+extends Node2D
+
+func _init():
+	pass
+
+func _on_TextureButton_mouse_enter():
+	print("_on_TextureButton_mouse_enter")
+
+func _on_TextureButton_mouse_exit():
+	print("_on_TextureButton_mouse_exit")
